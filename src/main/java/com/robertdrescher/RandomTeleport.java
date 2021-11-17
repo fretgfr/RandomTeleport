01a3c2a0495b241f487a35775cb83bd27e6a068d
Robert Drescher <robert@macombhometeam.com>
2021-11-17 00:55:56 -0500

    Initial Commit

---
+++ src/main/java/com/robertdrescher/RandomTeleport.java
@@ -0,0 +1,16 @@
+package com.robertdrescher;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RandomTeleport extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
